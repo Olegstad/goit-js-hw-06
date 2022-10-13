8cae2dab6d98bb6c83024f7d73abccc059a95630
Oleh Rudenko <olegstad@gmail.com>
2022-10-13 19:56:54 +0300

    Add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+ 
+    if (email.value === "" || password.value === "") {
+        alert("Please fill in all the fields!");
+    } else {
+        console.log({ 'Email': email.value, 'Password': password.value });
+        form.reset();
+    }
+});
+
+
+// 1. передаєм event параметром
+// 2. недаємо перезавантажити сторінку preventDefault
+// 3. отримуємо значення полів форми
+// 4. перевіряємо чи є пусті поля
+// 5. скидаємо форму
