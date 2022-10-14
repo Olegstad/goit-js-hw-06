44a00e575365cc34fae1b920e4edcb4f0b5a56bd
Oleh Rudenko <olegstad@gmail.com>
2022-10-14 13:08:38 +0300

    Add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorRef = document.querySelector('.color');
+
+const btnColorRef = document.querySelector('.change-color');
+
+const bgRef = document.querySelector('body');
+
+const onClick = () => { 
+  // console.dir(bgRef);
+
+  bgRef.style.backgroundColor = getRandomHexColor();
+
+  console.dir(colorRef);
+
+  colorRef.innerText = getRandomHexColor();
+}
+
+btnColorRef.addEventListener("click", onClick);
+
+
+
+
+
+
+// Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
+
+
+
+
+
+
